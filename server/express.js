216a366434d54bf96b7b6e5823ba7bee0cd7882d
Kausik.M <129713238+kausikrstnj@users.noreply.github.com>
2025-05-17 17:43:09 +0530

    server

---
+++ server/express.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const paperRoutes = require("./routes/paperRoutes");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+
+//app.use("/", userRoutes);
+app.use("/", authRoutes);
+app.use("/", paperRoutes);
+
+
+module.exports = app;
